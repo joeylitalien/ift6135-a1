568c9911bb478d73db32315e32b5fb29f8ac469a
joeylitalien <joey.litalien@gmail.com>
2018-02-01 23:32:54 -0500

    Fixed Problem 1 so it is consistent with what the TA expected us to do

--- mnist/utils.py
+++ mnist/utils.py
@@ -1,8 +1,28 @@
 import sys
 import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import TensorDataset
+import pickle
 
 
-def progress(count, total, status=''):
+def load_data(filename):
+    """ Load data into training/valid/test sets """
+
+    # Unpickle files (uses latin switch for py2.x to py3.x compatibility)
+    train, valid, test = pickle.load(open(filename, "rb"), encoding="latin1")
+    X_train, y_train = map(torch.from_numpy, train)
+    X_valid, y_valid = map(torch.from_numpy, valid)
+    X_test, y_test = map(torch.from_numpy, test)
+
+    # Convert to tensors
+    train_data = TensorDataset(X_train, y_train)
+    valid_data = TensorDataset(X_valid, y_valid)
+    test_data = TensorDataset(X_test, y_test)
+    
+    return train_data, valid_data, test_data
+
+
+def progress(count, total, status=""):
     """ Neat progress bar to track training """
 
     bar_len = 30
